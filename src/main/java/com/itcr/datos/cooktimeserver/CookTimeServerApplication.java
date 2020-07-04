c6f9efde29965d1cf6225681f2d073bf9e15392a
Isaac Herrera Monge <51173904+Isaacoun100@users.noreply.github.com>
2020-07-04 15:12:21 -0600

    Project Main
    This is the main that will run the server when initiated

---
+++ src/main/java/com/itcr/datos/cooktimeserver/CookTimeServerApplication.java
@@ -0,0 +1,13 @@
+package com.itcr.datos.cooktimeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookTimeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookTimeServerApplication.class, args);
+    }
+
+}
