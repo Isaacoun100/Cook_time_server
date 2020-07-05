df288542360fdce6c3f61d45ba2c5a868014af67
Isaacoun100 <Isaacoun100@gmail.com>
2020-07-05 13:34:47 -0600

    Javadoc added
    The CookTimeServerApplication is now commented, also once the server is initialized the server will read and load the users from the Json

--- src/main/java/com/itcr/datos/cooktimeserver/CookTimeServerApplication.java
+++ src/main/java/com/itcr/datos/cooktimeserver/CookTimeServerApplication.java
@@ -1,12 +1,21 @@
 package com.itcr.datos.cooktimeserver;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.itcr.datos.cooktimeserver.data.UserRepository;
+import org.springframework.boot.SpringApplication;
 
+/**
+ * CookTimeServerApplication is the main class, here will be executed only the neccesary for the code to run
+ */
 @SpringBootApplication
 public class CookTimeServerApplication {
 
+    /**
+     * This is the main method that will run the server and initialize the list of users.
+     * @param args
+     */
     public static void main(String[] args) {
+        UserRepository.initUserList();
         SpringApplication.run(CookTimeServerApplication.class, args);
     }
 
