a299c9151ab4a7346caaac8205bb3f15f23998b7
Isaacoun100 <Isaacoun100@gmail.com>
2020-07-05 13:30:21 -0600

    Restfull server
    The added methods belong to the basics of a Restfull server, with this method users can be created, visualized, added, and more.

--- src/main/java/com/itcr/datos/cooktimeserver/controller/HelloController.java
+++ src/main/java/com/itcr/datos/cooktimeserver/controller/HelloController.java
@@ -1,13 +1,40 @@
 package com.itcr.datos.cooktimeserver.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.itcr.datos.cooktimeserver.data_structures.SinglyList;
+import org.springframework.web.bind.annotation.*;
+import com.itcr.datos.cooktimeserver.data.UserRepository;
+import com.itcr.datos.cooktimeserver.object.User;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
-    public String getString(){
-        return "Hello WEBO";
+    public User getUserById(int userID) {
+        return UserRepository.getUserList().get(userID).getData();
     }
+    @GetMapping("/user")
+    public SinglyList<User> getString(){
+        UserRepository.updateUserList();
+        return UserRepository.getUserList();
+    }
+    @GetMapping("/user/{id}")
+    public User userID( @PathVariable int id ) {
+        try{
+            return getUserById(id);
+        }
+        catch(NullPointerException e){
+            return new User();
+        }
+    }
+    @PostMapping("newUser")
+    public User addUser( @RequestBody User newUser){
+        if(newUser!=null){
+            System.out.println(newUser.toString());
+            UserRepository.addUser(newUser);
+            return newUser;
+        }
+        else{
+            return new User();
+        }
+    }
+
 }
