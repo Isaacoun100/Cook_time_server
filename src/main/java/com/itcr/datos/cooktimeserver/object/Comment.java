0881c3391e3f450fe20729fe3eda73aa483a357a
Isaac Herrera Monge <51173904+Isaacoun100@users.noreply.github.com>
2020-07-12 19:20:12 -0600

    [Some fields were final] This final fields required no set option

--- src/main/java/com/itcr/datos/cooktimeserver/object/Comment.java
+++ src/main/java/com/itcr/datos/cooktimeserver/object/Comment.java
@@ -19,27 +19,10 @@ public class Comment {
     }
 
     /**
-     * Setter for the author
-     * @param author that is going to comment
-     */
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    /**
      * Getter for the comment
      * @return the recipe comment
      */
     public String getComment() {
         return comment;
     }
-
-    /**
-     * Setter for the comment
-     * @param comment that is going to be added
-     */
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
 }
