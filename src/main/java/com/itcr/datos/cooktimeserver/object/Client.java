6d0098644e845e5d06f461f05201b07952d71a01
Isaac Herrera Monge <51173904+Isaacoun100@users.noreply.github.com>
2020-07-12 19:19:10 -0600

    [New variables] The Client object was not fulfilling the requirements for the project

--- src/main/java/com/itcr/datos/cooktimeserver/object/Client.java
+++ src/main/java/com/itcr/datos/cooktimeserver/object/Client.java
@@ -1,5 +1,7 @@
 package com.itcr.datos.cooktimeserver.object;
 
+import com.itcr.datos.cooktimeserver.data_structures.SinglyList;
+
 public abstract class Client {
 
     /**
@@ -49,5 +51,92 @@ public abstract class Client {
      * @param age
      */
     public abstract void setAge(int age);
+
+    /**
+     * Getter for the String image
+     *
+     * @return image
+     */
+    public abstract String getImage();
+
+    /**
+     * Setter for the image url
+     *
+     * @param image that we're going to use
+     */
+    public abstract void setImage(String image);
+
+    /**
+     * Getter for the Singly List recipe
+     *
+     * @return recipe
+     */
+    public abstract SinglyList<Recipe> getRecipe();
+
+    /**
+     * Adds a new recipe to the recipe list
+     *
+     * @param recipe
+     */
+    public abstract void addRecipe(Recipe recipe);
+
+    /**
+     * Adds a new follower to the lis of followers
+     *
+     * @param name
+     */
+    public abstract void addFollower(String name);
+
+    /**
+     *Returns the list of followers
+     * @return follower
+     */
+    public abstract SinglyList<String> getFollowers();
+
+    /**
+     * Getter for the singly list following
+     *
+     * @return following
+     */
+    public abstract SinglyList<String> getFollowing();
+
+    /**
+     * This will add a new user or chef that the user is following
+     * @param name
+     */
+    public abstract void addFollowing(String name);
+
+    /**
+     * Will indicate whether or not the user owns a company
+     * @return hasCompany depending if he owns one
+     */
+    public abstract boolean isHasCompany();
+
+    /**
+     * Sets the hasCompany boolean
+     * @param hasCompany value if has or not
+     */
+    public abstract void setHasCompany(boolean hasCompany);
+
+    /**
+     * Setter for the recipe Singly list
+     *
+     * @param recipe that we're going to use
+     */
+    public abstract void setRecipe(SinglyList<Recipe> recipe);
+
+    /**
+     * Setter for the followers Singly list
+     *
+     * @param followers that we're going to use
+     */
+    public abstract void setFollowers(SinglyList<String> followers);
+
+    /**
+     * Setter for the following Singly list
+     *
+     * @param following is the singly list that is going to be added
+     */
+    public abstract void setFollowing(SinglyList<String> following);
 
 }
