b4bcec2093d64f37c58bf7799b115bb9046cee50
NJohnsonCR <60128330+NJohnsonCR@users.noreply.github.com>
2020-07-18 12:10:33 -0600

    Update SortingAlgorithms.java

--- src/main/java/com/itcr/datos/cooktimeserver/sorting/SortingAlgorithms.java
+++ src/main/java/com/itcr/datos/cooktimeserver/sorting/SortingAlgorithms.java
@@ -1,6 +1,7 @@
 package com.itcr.datos.cooktimeserver.sorting;
 
 import com.itcr.datos.cooktimeserver.data_structures.*;
+import com.itcr.datos.cooktimeserver.object.DifficultySort;
 import com.itcr.datos.cooktimeserver.object.RatingSort;
 
 import java.util.Arrays;
@@ -204,4 +205,60 @@ public class SortingAlgorithms {
             list.get(i).setData(output[i]);
 
     }
+    /**
+     * The main function that sorts the singly list
+     * @param list the integer singly list
+     */
+    public static void radix_sort_difficulty(SinglyList<DifficultySort> list){
+        int length = list.getLength();
+
+        int max = get_max_difficulty(list, length);
+
+        for (int exp = 1; max/exp > 0; exp *= 10)
+            count_sort_difficulty(list, length, exp);
+
+    }
+
+    /**
+     * A utility function to get maximum value in singly list
+     * @param list the singly list
+     * @param lenght the lenght
+     * @return returns the maximum value
+     */
+    private static int get_max_difficulty(SinglyList<DifficultySort> list, int lenght){
+        int max = list.getHead().getData().getDifficulty();
+        for (int i = 1; i < lenght; i++)
+            if (list.get(i).getData().getDifficulty() > max)
+                max = list.get(i).getData().getDifficulty();
+        return max;
+    }
+    /**
+     * A function to do counting sort of SinglyList according to the digit represented by exp
+     * @param list the singly list
+     * @param lenght the length
+     * @param exp the exponent
+     */
+    private static void count_sort_difficulty(SinglyList<DifficultySort> list, int lenght, int exp){
+        int[] output = new int[lenght];
+        int i;
+        int[] count = new int[10];
+        Arrays.fill(count, 0);
+
+
+        for (i = 0; i < lenght; i++)
+            count[ (list.get(i).getData().getDifficulty()/exp) % 10 ]++;
+
+        for (i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        for (i = lenght - 1; i >= 0; i--)
+        {
+            output[count[ (list.get(i).getData().getDifficulty()/exp)%10 ] - 1] = list.get(i).getData().getDifficulty();
+            count[ (list.get(i).getData().getDifficulty()/exp)%10 ]--;
+        }
+
+        for (i = 0; i < lenght; i++)
+            list.get(i).getData().setDifficulty(output[i]);
+    }
+
 }
