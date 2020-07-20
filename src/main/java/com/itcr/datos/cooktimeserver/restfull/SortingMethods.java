a2c7f1f60b6dfca939a39a732166dde1e0f28f2a
NJohnsonCR <60128330+NJohnsonCR@users.noreply.github.com>
2020-07-19 23:04:05 -0600

    Update SortingMethods.java

--- src/main/java/com/itcr/datos/cooktimeserver/restfull/SortingMethods.java
+++ src/main/java/com/itcr/datos/cooktimeserver/restfull/SortingMethods.java
@@ -74,6 +74,52 @@ public class SortingMethods {
 
         return datesort_list;
     }
+    public static SinglyList<DateSort> DateSortUser(String user){
+        return DateSortUser(user, new SinglyList<>());
+    }
+    private static SinglyList<DateSort> DateSortUser(String user, SinglyList<Recipe> list){
+        AlphNodeAVL<Recipe> recipeAlphNodeAVL = TreeManagement.BinarySearchAvl(user);
+        list.add(recipeAlphNodeAVL.getData());
+
+        SinglyList<DateSort> dateSortSinglyList = new SinglyList<>();
+
+        for (int i = 0; i < list.getLength(); i++){
+            String title = list.get(i).getData().getTitle();
+            String dates = list.get(i).getData().getDate();
+            dateSortSinglyList.add(new DateSort(title, dates));
+        }
+        SinglyList<Integer> days = new SinglyList<>();
+        SinglyList<Integer> months = new SinglyList<>();
+        SinglyList<Integer> years = new SinglyList<>();
+
+        for (int i = 0; i < dateSortSinglyList.getLength(); i++){
+            String date = dateSortSinglyList.get(i).getData().getDate();
+
+            days.add(Integer.parseInt(String.valueOf(date.charAt(0)) + date.charAt(1)));
+
+            months.add(Integer.parseInt(String.valueOf(date.charAt(3)) + date.charAt(4)));
+
+            years.add(Integer.parseInt(String.valueOf(date.charAt(6)) + date.charAt(7) + date.charAt(8) + date.charAt(9)));
+
+
+        }
+        SortingAlgorithms.bubble_sort(days);
+        SortingAlgorithms.bubble_sort(months);
+        SortingAlgorithms.bubble_sort(years);
+        for (int i = 0; i < dateSortSinglyList.getLength(); i++){
+            String dayString = Integer.toString(days.get(i).getData());
+            String monthString = Integer.toString(months.get(i).getData());
+            String yearString = Integer.toString(years.get(i).getData());
+
+            dateSortSinglyList.get(i).getData().setDate(dayString + "/" + monthString + "/" + yearString);
+        }
+        dateSortSinglyList = dateSortSinglyList.Inverter(dateSortSinglyList);
+        dateSortSinglyList.print_list();
+
+        return dateSortSinglyList;
+
+    }
+
 
     /**
      * Function for doing a binary search of the ratings with its titles
@@ -105,10 +151,40 @@ public class SortingMethods {
         SinglyList<RatingSort> list;
         list = BinarySearchRating();
         SortingAlgorithms.quick_sort_ratings(list);
-        list.Inverter(list);
+        list = list.Inverter(list);
         list.print_list();
 
         return list;
+    }
+
+    /**
+     * Function that sorts the recipes of the specified user
+     * @param user the user id
+     * @return returns the recursive function
+     */
+    public static SinglyList<RatingSort> ratingSortUser(String user){
+        return RatingSortUser( user, new SinglyList<>());
+    }
+
+    /**
+     * Function that sorts the ratings of the recipes of each user
+     * @param user the user id
+     * @param list the recipe singly list
+     * @return returns the new singly list sorted
+     */
+    private static SinglyList<RatingSort> RatingSortUser(String user, SinglyList<Recipe> list){
+        AlphNodeAVL<Recipe> recipeAlphNodeAVL = TreeManagement.BinarySearchAvl(user);
+        list.add(recipeAlphNodeAVL.getData());
+
+        SinglyList<RatingSort> ratingSortSinglyList = new SinglyList<>();
+        for (int i = 0; i < list.getLength(); i++){
+            String title = list.get(i).getData().getTitle();
+            int ratings = list.get(i).getData().getRating();
+            ratingSortSinglyList.add(new RatingSort(title, ratings));
+        }
+        SortingAlgorithms.quick_sort_ratings(ratingSortSinglyList);
+        ratingSortSinglyList = ratingSortSinglyList.Inverter(ratingSortSinglyList);
+        return ratingSortSinglyList;
     }
     /**
      * Function for doing a binary search of the ratings with its titles
@@ -144,4 +220,35 @@ public class SortingMethods {
 
         return list;
     }
+
+    /**
+     * Function that sorts the recipes of the specified user
+     * @param user the user id
+     * @return returns the recursive function
+     */
+    public static SinglyList<DifficultySort> DifficultySortUser(String user){
+        return DifficultySortUser( user, new SinglyList<>());
+    }
+
+    /**
+     * Function that sorts the ratings of the recipes of each user
+     * @param user the user id
+     * @param list the recipe singly list
+     * @return returns the new singly list sorted
+     */
+    private static SinglyList<DifficultySort> DifficultySortUser(String user, SinglyList<Recipe> list){
+        AlphNodeAVL<Recipe> recipeAlphNodeAVL = TreeManagement.BinarySearchAvl(user);
+        list.add(recipeAlphNodeAVL.getData());
+
+        SinglyList<DifficultySort> difficultySortSinglyList = new SinglyList<>();
+        for (int i = 0; i < list.getLength(); i++){
+            String title = list.get(i).getData().getTitle();
+            int difficulties = list.get(i).getData().getRating();
+            difficultySortSinglyList.add(new DifficultySort(title, difficulties));
+        }
+        SortingAlgorithms.radix_sort_difficulty(difficultySortSinglyList);
+        difficultySortSinglyList = difficultySortSinglyList.Inverter(difficultySortSinglyList);
+        return difficultySortSinglyList;
+    }
+
 }
